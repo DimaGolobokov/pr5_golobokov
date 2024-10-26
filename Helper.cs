7f2abc98493a80df14cd51f729cf78d6de50443d
Dmitry <sedbay@mail.ru>
2024-10-26 19:13:31 +0700

    Add project files.

---
+++ Helper.cs
@@ -0,0 +1,26 @@
+﻿using pr5_golobokov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr5_golobokov
+{
+    internal class Helper
+    {
+        private static InsuranceMedicalCompanyDBEntities _context; // Создание статичной приватной переменной, для обращения к контексту модели данных
+
+        // Метод (публичный, чтобы к нему можно было обращаться из любой части программы)
+        // получения контекста данных, необходимый для создания подключения к БД
+        public static InsuranceMedicalCompanyDBEntities GetContext()
+        {
+            // Условие при котором проверяется, что если подключение не установлено, то необходимо создать новое подключение
+            if (_context == null)
+            {
+                _context = new InsuranceMedicalCompanyDBEntities(); // Создание нового подключения к БД
+            }
+            return _context; // возвращение подключения
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using pr5_golobokov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HashPasswords;
+
+namespace pr5_golobokov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Создание новой учетной записи для пользователя");
+
+            Console.Write("Введите логин пользователя: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Введите пароль пользователя: ");
+            string password = Console.ReadLine();
+
+            Hash hashHelper = new Hash();
+            string hashedPassword = hashHelper.HashPassword(password);
+
+            Auth newUser = new Auth
+            {
+                Username = username,
+                Password = hashedPassword
+            };
+
+            try
+            {
+                var context = Helper.GetContext();
+                context.Auth.Add(newUser);
+                context.SaveChanges();
+
+                Console.WriteLine($"Хэшированный пароль пользователя: {hashedPassword}");
+                Console.WriteLine("Учетная запись добавлена");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при добавлении учетной записи: {ex.Message}");
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для завершения...");
+            Console.ReadKey();
+        }
+    }
+}
